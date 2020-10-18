d395ecd59643e162e6b5775d6cce24c52cb998f0
rado <radoosredkar@hotmail.com>
2020-10-18 17:43:11 +0200

    Add cors

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_graphql import GraphQLView
 from schemas import schema
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import scrappy
+import logging
 
-# import sentry_sdk
-# sentry_sdk.init("https://007e055e5fe64e35b55b36140bf6b18d@o371271.ingest.sentry.io/5363923")
+import sentry_sdk
 
+sentry_sdk.init(
+    "https://007e055e5fe64e35b55b36140bf6b18d@o371271.ingest.sentry.io/5363923"
+)
 app = Flask(__name__)
-CORS(app)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+cors = CORS(app)
+# cors = CORS(app, resources={r"/*": {"origins": "http://localhost:8080/"}})
+app.logger.info(cors)
 app.add_url_rule(
     "/graphql", view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True)
 )
@@ -19,7 +23,7 @@ app.add_url_rule(
 )
 
 
-@app.route("/refresh")
+@app.route("/refreshq")
 def refresh():
     scrappy.scrapp()
     scrappy.scrappNepremicnine()
@@ -28,10 +32,20 @@ def refresh():
 
 
 if __name__ == "__main__":
+    logging.getLogger("flask_cors").level = logging.DEBUG
+    logging.getLogger("flask").level = logging.DEBUG
+    logging.info("test")
+
     app.run(debug=True, host="0.0.0.0")
 
 
 @app.before_request
 def log_request_info():
+    print(1342)
     app.logger.debug("Headers: %s", request.headers)
     app.logger.debug("Body: %s", request.get_data())
+
+
+@app.after_request
+def add_headers(response):
+    print(1342)
