991eafd8dd580ce6f9b7d5708ab0e8dcc27be83d
rado <radoosredkar@hotmail.com>
2020-12-27 12:23:32 +0100

    Refactor submodules

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import ipdb
 from flask import Flask
 from flask import request
 from flask_graphql import GraphQLView
 from flask_cors import CORS, cross_origin
-from web_collector.schemas import schema
-from web_collector import scrappy
-from web_collector import ParserBolha as bolha
-from web_collector import ParserNepremicnine as nepremicnine
+from web_collector import schemas
+import scrappy
+from web_collector.scrapper import ParserBolha as bolha
+from web_collector.scrapper import ParserNepremicnine as nepremicnine
 import logging
 
 import sentry_sdk
@@ -17,22 +18,23 @@ sentry_sdk.init(
     traces_sample_rate=1.0,
 )
 
+
 app = Flask(__name__)
 cors = CORS(app)
 # cors = CORS(app, resources={r"/*": {"origins": "http://localhost:8080/"}})
 app.logger.info(cors)
 app.add_url_rule(
-    "/graphql", view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True)
+    "/graphql", view_func=GraphQLView.as_view("graphql", schema=schemas.schema, graphiql=True)
 )
 app.add_url_rule(
     "/graphql/batch",
-    view_func=GraphQLView.as_view("graphqlb", schema=schema, batch=True, method="POST"),
+    view_func=GraphQLView.as_view("graphqlb", schema=schemas.schema, batch=True, method="POST"),
 )
 
 
 @app.route("/refresh")
 def refresh():
-    print(1 / 0)
+    # print(1 / 0)
     return scrappy.refresh()
 
 
@@ -41,7 +43,7 @@ if __name__ == "__main__":
     logging.getLogger("flask").level = logging.DEBUG
     logging.info("test")
 
-    # app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
 
 
 # @app.before_request
