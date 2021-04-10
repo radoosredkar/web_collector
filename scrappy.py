e12be65b66fd9de4ec4cd12447fd26cc2303abe9
rado <radoosredkar@hotmail.com>
2021-04-10 18:26:07 +0200

    Update filters

--- scrappy.py
+++ scrappy.py
@@ -14,9 +14,9 @@ from random import randrange
 # import sentry_sdk
 # sentry_sdk.init("https://007e055e5fe64e35b55b36140bf6b18d@o371271.ingest.sentry.io/5363923")
 
-url_bolha = "https://www.bolha.com/index.php?ctl=search_ads&keywords=stanovanja&categoryId=9580&price[min]=98000&price[max]=140999&level0LocationId%5B26320%5D=26320&sort=new&page={page}"
+url_bolha = "https://www.bolha.com/index.php?ctl=search_ads&keywords=stanovanja&categoryId=9580&price[min]=98000&price[max]=200000&level0LocationId%5B26320%5D=26320&sort=new&page={page}"
 
-url_nepremicnine = "https://www.nepremicnine.net/oglasi-prodaja/ljubljana-okolica/stanovanje/cena-od-100000-do-150000-eur,velikost-od-50-do-100-m2/{page}/"
+url_nepremicnine = "https://www.nepremicnine.net/oglasi-prodaja/ljubljana-okolica/stanovanje/cena-od-100000-do-200000-eur,velikost-od-50-do-100-m2/{page}/"
 
 
 def refresh():
@@ -32,18 +32,19 @@ def refresh():
 
     all_changed_items = 0
     app.logger.info("Refreshing bolha")
-    # all_changed_items = all_changed_items + bolha.scrapp(url_bolha)
+    all_changed_items = all_changed_items + bolha.scrapp(url_bolha)
     app.logger.debug(f"Bolha refreshed {all_changed_items}")
 
     app.logger.info("Refreshing nepremicnine")
-    # all_changed_items = all_changed_items + nepremicnine.scrapp(url_nepremicnine)
+    all_changed_items = all_changed_items + nepremicnine.scrapp(url_nepremicnine)
     app.logger.debug(f"Nepremicnine refreshed {all_changed_items}")
 
     db_firestore.update_document(doc_ref, {"changed_items": all_changed_items})
     homes_coll = db_firestore.get_collection(settings.collections.homes)
     for home in homes_coll:
         parsed = home.to_dict()
-        timedelta = parsed["date_found"] - parsed["date_created"]
+        timedelta = now - parsed["date_found"].replace(tzinfo=None)
+        # app.logger.info("timedelta %s,%s", timedelta.days, settings.db.age_to_archive_days)
         if timedelta.days > settings.db.age_to_archive_days:
             app.logger.debug("Archieving document with timedelta %s", timedelta)
             db_firestore.update_document(
